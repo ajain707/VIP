7d059aed785ff2f841243a2d4dc6a51b967fd319
Akshat-Jain47 <ajain707@gatech.edu>
2023-02-24 11:27:42 -0500

    first commit

---
+++ RandomForrest.py
@@ -0,0 +1,8 @@
+# This is a random forrest algorithm used for terrain classification 
+# Load training Data 
+
+# Split training data into subsets randomly (sampling with replacement)
+
+# Make a decision tree for each subset 
+
+# Classify the training example based on majority of the output of decision trees 
